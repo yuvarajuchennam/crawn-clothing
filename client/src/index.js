d9409ab7f89795cdf249fbf78af1193c4882dc3a
yuvarajuchennam <chennamyuvraju1@gmail.com>
2020-06-04 18:22:07 +0530

    stripe payment

---
+++ client/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+// persist
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './redux/store';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <React.StrictMode>
+//         <App />
+//       </React.StrictMode>
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById('root')
+// );
+
+
+//persist
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- src/redux/user/user.selector.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectUser = store => store.user;
-
-export const selectCurrentUser = createSelector(
-    [selectUser],
-    user => user.currentUser
-)
